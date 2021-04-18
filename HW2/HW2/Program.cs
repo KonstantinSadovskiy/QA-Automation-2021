43a69801f677dc770ce9aa7038a1d535520fcd0a
k0styamba <kostiasadovskiy@gmail.com>
2021-04-18 18:41:42 +0300

    1+2 Compl

--- HW2/HW2/Program.cs
+++ HW2/HW2/Program.cs
@@ -6,7 +6,72 @@ namespace HW2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Consecutive: " + Consecutive(args[0]));
+            Console.WriteLine("NonConsecutive: " + NonConsecutive(args[0]));
+        }
+
+        static int Consecutive(string str)
+        {
+            int maxCount = 0;
+            int tempCount = 1;
+
+            for(int i = 1; i < str.Length; i++) //122315655458
+            {
+                tempCount++;
+
+                if (str[i - 1] == str[i])
+                {
+                    tempCount = 1;
+                }
+
+                if (tempCount > maxCount)
+                {
+                    maxCount = tempCount;
+                }
+            }
+
+            return maxCount;
+        }
+
+        static int NonConsecutive(string str)
+        {
+            int maxCount = 0;
+            int tempCount = 1;
+            string tempArr = "";
+
+            int i = 1;
+            do
+            {
+                tempCount++;
+
+                tempArr = tempArr.Insert(0, str[(i - 1)..i]);
+
+                for (int j = 0; j < tempArr.Length; j++)
+                {
+                    if (tempArr[j] == str[i])
+                    {
+                        tempCount = 1;
+
+                        i = 1 + str.IndexOf(str[i]);
+                        str = str.Substring(i);
+                        i = 0;
+
+                        tempArr = "";
+
+                        break;
+                    }
+                }
+
+                if (tempCount > maxCount)
+                {
+                    maxCount = tempCount;
+                }
+
+                i++;
+            }
+            while (i < str.Length);
+
+            return maxCount;
         }
     }
 }
